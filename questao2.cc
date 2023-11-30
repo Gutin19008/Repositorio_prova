174b4cce5921cccdf97e999cce61b62972ddf57a
Gutin <142357546+Gutin19008@users.noreply.github.com>
2023-11-29 21:51:51 -0300

    Add files via upload

---
+++ questao2.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int num1, num2, result;
+	printf("digite um numero: \n");
+	scanf("%d",num1);
+	printf("digite outro numero: \n");
+	scanf("%d",num2);
+	
+	if(num1 > num2){
+	    num1+num2=result;
+		
+		printf("a soma dos dois numeros são",result );
+	}
+}
+	else
+}
